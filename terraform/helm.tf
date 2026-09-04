3a2726290b33f98e2e0399629b377d9fb1f4c5f0
Itay Boguslavsky <itay.bogu@gmail.com>
2026-09-04 17:35:49 +0300

    Fixed terraform syntax

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,10 +1,10 @@
 # Authenticate Terraform's Helm provider with the EKS cluster
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "us-east-1"]
@@ -19,6 +19,5 @@ resource "helm_release" "traefik" {
   chart            = "traefik"
   namespace        = "kube-system"
   
-  # Ensure the node group is ready before installing Traefik
   depends_on = [module.eks]
 }
