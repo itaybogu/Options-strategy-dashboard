3188b9b984561c8124360d5a46a4cd290e5ca039
Itay Boguslavsky <itay.bogu@gmail.com>
2026-09-04 18:54:23 +0300

    Added security rules in firewall to allow metrics monitoring

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,17 +2,35 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.36"
+  cluster_name                             = var.cluster_name
+  cluster_version                          = "1.36"
   enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
 
+  node_security_group_additional_rules = {
+    ingress_metrics_server_10251 = {
+      description                   = "Cluster API to node for metrics server"
+      protocol                      = "tcp"
+      from_port                     = 10251
+      to_port                       = 10251
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+    ingress_metrics_server_4443 = {
+      description                   = "Cluster API to node for metrics server standard port"
+      protocol                      = "tcp"
+      from_port                     = 4443
+      to_port                       = 4443
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+  }
 
-cluster_addons = {
+  cluster_addons = {
     metrics-server = {
       most_recent = true
     }
@@ -27,8 +45,7 @@ cluster_addons = {
     }
   }
 
-
-eks_managed_node_groups = {
+  eks_managed_node_groups = {
     app_nodes = {
       min_size     = 1
       max_size     = 2
@@ -42,8 +59,6 @@ eks_managed_node_groups = {
       }
     }
   }
-
-
 
   tags = {
     Environment = var.environment
